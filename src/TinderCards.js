76f716f2e68e026b764df503c44878b850b3816e
ghost <fayyad_r22@yahoo.com>
2021-03-14 07:59:53 +0100

    add contener to all cards and add tindercard from react tinder card add function to swip and style the cards

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./TinderCards.css";
-import TinderCards from "react-tinder-card";
+import TinderCard from "react-tinder-card";
+import { SwipeableDrawer } from "@material-ui/core";
 
 function TinderCards() {
   const [people, usePeople] = useState([
@@ -15,11 +16,32 @@ function TinderCards() {
         "https://www.biography.com/.image/t_share/MTY2NzA3ODE3OTgwMzcyMjYw/jeff-bezos-andrew-harrer_bloomberg-via-getty-images.jpg",
     },
   ]);
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing" + nameToDelete);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + "left the screen");
+  };
   return (
     <div className="tinderCards">
       <div className="tinderCards__cardContainer">
         {people.map((person) => (
-          <TinderCards></TinderCards>
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwip={["up", "down"]}
+            onSwipe={(dir) => SwipeableDrawer(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          >
+            <div
+              style={{ backgroundImage: `url(${person.url})` }}
+              className="card"
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
         ))}
       </div>
     </div>
